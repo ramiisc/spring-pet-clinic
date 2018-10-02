d3cc971da9e259659fde3d95af99b4eafc773a2f
Ramakrishna Potluru <prk.iisc@gmail.com>
2018-10-02 12:38:44 +0530

    Add ID for model objects. Closes #10

--- pet-clinic-data/src/main/java/com/rpotluru/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/rpotluru/springpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.rpotluru.springpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 	private String firstName;
 	private String lastname;
 
--- pet-clinic-data/src/main/java/com/rpotluru/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/rpotluru/springpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.rpotluru.springpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
 	private PetType PetType;
 	private Owner owner;
 	private LocalDate birthDate;
