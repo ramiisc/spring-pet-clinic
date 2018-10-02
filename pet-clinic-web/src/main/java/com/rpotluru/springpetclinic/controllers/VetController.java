07a8f79f99721c1369ed90918243c63e41d3ed5b
Ramakrishna Potluru <prk.iisc@gmail.com>
2018-10-02 20:46:50 +0530

    Create Vet index page and Controller. CLoses #7

---
+++ pet-clinic-web/src/main/java/com/rpotluru/springpetclinic/controllers/VetController.java
@@ -0,0 +1,17 @@
+package com.rpotluru.springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("vets")
+public class VetController {
+	
+	@RequestMapping({"","/","index","index.html"})
+	public String listVets() {
+		return "vets/index";
+		
+	}
+	
+
+}
