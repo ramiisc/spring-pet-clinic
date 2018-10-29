376a1241095be5b9ced6db4c29ff95f18652e863
Ramakrishna Potluru <prk.iisc@gmail.com>
2018-10-29 14:55:53 +0530

    Create Pet type Map service Impl. Closes #29

---
+++ pet-clinic-data/src/main/java/com/rpotluru/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.rpotluru.springpetclinic.services;
+
+import com.rpotluru.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+}
---
+++ pet-clinic-data/src/main/java/com/rpotluru/springpetclinic/services/map/PetTypeMap.java
@@ -0,0 +1,38 @@
+package com.rpotluru.springpetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.rpotluru.springpetclinic.model.PetType;
+import com.rpotluru.springpetclinic.services.PetTypeService;
+
+
+@Service
+public class PetTypeMap extends AbstractMapService<PetType, Long> implements PetTypeService{
+	@Override
+	public Set<PetType> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public PetType findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public PetType save(PetType object) {
+		super.save(object.getId(), object);
+		return object;
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(PetType object) {
+		super.delete(object);
+	}
+}
