cd268e9f957ae57bb8ce5b8cffe44d589d463134
Ramakrishna Potluru <prk.iisc@gmail.com>
2018-10-29 10:42:18 +0530

    Create vet specialty entity and associate to Vet. CLose #27

--- pet-clinic-data/src/main/java/com/rpotluru/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/rpotluru/springpetclinic/model/Vet.java
@@ -3,10 +3,25 @@
  */
 package com.rpotluru.springpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author rpotluru
  *
  */
 public class Vet extends Person {
+	
+	private Set<Specialty> Specialties;
+
+	public Set<Specialty> getSpecialties() {
+		return Specialties;
+	}
+
+	public void setSpecialties(Set<Specialty> specialties) {
+		Specialties = specialties;
+	}
+	
+	
+	
 
 }
